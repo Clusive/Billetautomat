37c11bc6f04dbd9906d7196df5b08779c73b47e6
bøllemis <Bruger@Acer-PC>
2015-10-22 17:30:45 +0200

    Baseline

---
+++ src/automat/BilletautomatTest.java
@@ -0,0 +1,29 @@
+package automat;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BilletautomatTest {
+
+	@Test
+	public void testIndsætPenge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMontørLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBilletpris() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNulstil() {
+		fail("Not yet implemented");
+	}
+
+}
